6b451cec89d076a8571e5c926a3f6d4115ad1f06
Thuru <thurucool@hotmail.com>
2018-07-01 14:50:39 +0530

    asp.net core project doesn't get the dv. check startup.cs line 34

---
+++ LayeredProject/LayeredProject/Startup.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LayeredProject.DataContext;
+using LayeredProject.Services.BusinessServices;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace LayeredProject
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            // This is right
+            services.AddSingleton<IProductService, ProductService>();
+
+            // This should fail but it works
+            services.AddSingleton<IMyDbContext, MyDbContext>();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+        }
+    }
+}
